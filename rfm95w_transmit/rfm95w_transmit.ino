ef6e0733d6afc518fa0b7f6bf3a5e278ce89a67f
Fraser Crossman <frasercrossman@outlook.com>
2018-03-09 16:20:47 +0000

    Switched to using Arduino-LMIC library Arduino-LMIC: https://github.com/matthijskooijman/arduino-lmic

--- rfm95w_transmit/rfm95w_transmit.ino
+++ rfm95w_transmit/rfm95w_transmit.ino
@@ -5,6 +5,10 @@
 #include <TinyGPS++.h>
 #include <RTClock.h>
 
+#include <lmic.h>
+#include <hal/hal.h>
+#include <SPI.h>
+
 #include "packet_constructor.hpp"
 
 // The TinyGPS++ object
@@ -23,127 +27,104 @@ Adafruit_Simple_AHRS ahrs(&accel, &mag);
 // Orientation vector
 sensors_vec_t orientation;
 
-/*
- * Based on work by Thomas Telkamp
- * Available at: https://github.com/tftelkamp/sx127x_tx_rx
- */
-
-#define SSPIN   PA4  // 10 for mjs board
-#define DIO0    PB1  //  2 for mjs board
+#define SSPIN   PA4
+#define DIO0    PB1
 #define RST     PB0
 
-#define TXDELAY 1000  // in milliseconds
+// LoRaWAN NwkSKey, network session key
+static const PROGMEM u1_t NWKSKEY[16] = { 0xF7, 0x4B, 0x10, 0x96, 0xF0, 0xD3, 0xDf, 0x8B, 0x72, 0xC5, 0xA3, 0x99, 0x19, 0xA4, 0xAF, 0x73 };
 
-/* --------------------------- */
+// LoRaWAN AppSKey, application session key
+static const u1_t PROGMEM APPSKEY[16] = { 0x00, 0xC7, 0xCA, 0x42, 0xC8, 0x46, 0x7B, 0x39, 0xE0, 0xF2, 0x52, 0xFE, 0x72, 0x47, 0x07, 0x4E };
 
-// Set center frequency
-uint32_t  freq = 868100000; // in Mhz! (868.1)
+// LoRaWAN end-device address
+static const u4_t DEVADDR = 0x06BF10C8;
 
-// Set tx payload
-char payload[] = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// These callbacks are only used in over-the-air activation, so they are
+// left empty here (we cannot leave them out completely unless
+// DISABLE_JOIN is set in config.h, otherwise the linker will complain).
+void os_getArtEui (u1_t* buf) { }
+void os_getDevEui (u1_t* buf) { }
+void os_getDevKey (u1_t* buf) { }
 
-int payloadlength = sizeof(payload);
-char message[256];
+unsigned char custom_payload[128];
+static osjob_t sendjob;
 
-/* --------------------------- */
+// Schedule TX every this many seconds (might become longer due to duty
+// cycle limitations).
+const unsigned TX_INTERVAL = 60;
 
-// Register mappings also apply to RFM95 devices
-#include "sx1276Regs-LoRa.h"
+// Pin mapping
+const lmic_pinmap lmic_pins = {
+    .nss = PA4,
+    .rxtx = LMIC_UNUSED_PIN,
+    .rst = RST,
+    .dio = {DIO0, PB10, PB11},
+};
 
-#include <SPI.h>
-
-// MODES
-#define MODE_RX_CONTINOUS (RFLR_OPMODE_LONGRANGEMODE_ON | RFLR_OPMODE_RECEIVER)    // 0x85
-#define MODE_TX           (RFLR_OPMODE_LONGRANGEMODE_ON | RFLR_OPMODE_TRANSMITTER) // 0x83
-#define MODE_SLEEP        (RFLR_OPMODE_LONGRANGEMODE_ON | RFLR_OPMODE_SLEEP)       // 0x80
-#define MODE_STANDBY      (RFLR_OPMODE_LONGRANGEMODE_ON | RFLR_OPMODE_STANDBY)     // 0x81
-
-#define FRF_MSB           0xD9 // 868.1 Mhz (see datasheet)
-#define FRF_MID           0x06
-#define FRF_LSB           0x66
-
-#define LNA_MAX_GAIN      (RFLR_LNA_GAIN_G1 | RFLR_LNA_BOOST_HF_ON) // 0x23
-#define LNA_OFF_GAIN      RFLR_LNA_BOOST_HF_OFF
-
-
-
-#define PA_MAX_BOOST      0x8F
-#define PA_LOW_BOOST      0x81
-#define PA_MED_BOOST      0x8A
-#define PA_OFF_BOOST      0x00
-
-#define MODEMCONFIG1      (RFLR_MODEMCONFIG1_IMPLICITHEADER_OFF | RFLR_MODEMCONFIG1_CODINGRATE_4_5 | RFLR_MODEMCONFIG1_BW_125_KHZ)
-#define MODEMCONFIG2      (RFLR_MODEMCONFIG2_SF_8 | RFLR_MODEMCONFIG2_TXCONTINUOUSMODE_OFF| RFLR_MODEMCONFIG2_RXPAYLOADCRC_ON)
-
-void setup() {
-    // initialise the pins
-
-    // LED flash
-    pinMode(PC13, OUTPUT);
-    digitalWrite(PC13, LOW);
-    delay(1000);
-    digitalWrite(PC13, HIGH);
-    delay(2000);
-
-    pinMode(SSPIN, OUTPUT);
-    pinMode(DIO0,  INPUT_PULLDOWN);
-    pinMode(RST,  INPUT_PULLUP);
-    digitalWrite(RST, LOW);
-    delay(100);
-    digitalWrite(RST, HIGH);
-    delay(100);
-    Serial.begin(9600);
-    Serial1.begin(9600);
-
-    SPI.begin();
-    SPI.setBitOrder(MSBFIRST);            // Set the SPI_1 bit order
-    SPI.setDataMode(SPI_MODE0);           // Set the  SPI_2 data mode 0
-    SPI.setClockDivider(SPI_CLOCK_DIV16); // Slow speed (72 / 16 = 4.5 MHz SPI_1 speed)
-
-    Serial.println(readRegister(0x42));
-    // LoRa mode
-    writeRegister(REG_LR_OPMODE,MODE_SLEEP);
-
-    // Frequency
-    //writeRegister(REG_LR_FRFMSB,FRF_MSB);
-    //writeRegister(REG_LR_FRFMID,FRF_MID);
-    //writeRegister(REG_LR_FRFLSB,FRF_LSB);
-
-    // Frequency
-    uint64_t frf = ((uint64_t)freq << 19) / 32000000;
-    writeRegister(REG_LR_FRFMSB, (uint8_t)(frf>>16) );
-    writeRegister(REG_LR_FRFMID, (uint8_t)(frf>> 8) );
-    writeRegister(REG_LR_FRFLSB, (uint8_t)(frf>> 0) );
-
-    // Turn on implicit header mode and set payload length
-    writeRegister(REG_LR_MODEMCONFIG1, MODEMCONFIG1);
-    writeRegister(REG_LR_MODEMCONFIG2, MODEMCONFIG2);
-    writeRegister(REG_LR_PARAMP,RFLR_PARAMP_0050_US);
-    writeRegister(REG_LR_PAYLOADLENGTH, 128);
-    writeRegister(REG_LR_SYNCWORD,0x34);  // LoRaWAN Public = 0x34, Private = 0x12
-
-    // Change the DIO mapping to 01 so we can listen for TxDone on the interrupt
-    writeRegister(REG_LR_DIOMAPPING1, RFLR_DIOMAPPING1_DIO0_01);
-
-    // Go to standby mode
-    writeRegister(REG_LR_OPMODE,MODE_STANDBY);
-
-    Serial.println("Device Setup Complete");
-
-    // Initialise the sensors.
-    accel.begin();
-    mag.begin();
-    // Initialise batter analogue read
-    pinMode(PA0, INPUT_ANALOG);
-
-    Serial.println("Sensor Initialisation Complete");
-}
-
-void loop() {
-    delay(1000);
-    //Serial.print(".");
-    //getData();
-    txloop();
+void onEvent (ev_t ev) {
+    Serial.print(os_getTime());
+    Serial.print(": ");
+    switch(ev) {
+        case EV_SCAN_TIMEOUT:
+            Serial.println(F("EV_SCAN_TIMEOUT"));
+            break;
+        case EV_BEACON_FOUND:
+            Serial.println(F("EV_BEACON_FOUND"));
+            break;
+        case EV_BEACON_MISSED:
+            Serial.println(F("EV_BEACON_MISSED"));
+            break;
+        case EV_BEACON_TRACKED:
+            Serial.println(F("EV_BEACON_TRACKED"));
+            break;
+        case EV_JOINING:
+            Serial.println(F("EV_JOINING"));
+            break;
+        case EV_JOINED:
+            Serial.println(F("EV_JOINED"));
+            break;
+        case EV_RFU1:
+            Serial.println(F("EV_RFU1"));
+            break;
+        case EV_JOIN_FAILED:
+            Serial.println(F("EV_JOIN_FAILED"));
+            break;
+        case EV_REJOIN_FAILED:
+            Serial.println(F("EV_REJOIN_FAILED"));
+            break;
+        case EV_TXCOMPLETE:
+            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+            if (LMIC.txrxFlags & TXRX_ACK)
+              Serial.println(F("Received ack"));
+            if (LMIC.dataLen) {
+              Serial.println(F("Received "));
+              Serial.println(LMIC.dataLen);
+              Serial.println(F(" bytes of payload"));
+            }
+            // Schedule next transmission
+            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
+            break;
+        case EV_LOST_TSYNC:
+            Serial.println(F("EV_LOST_TSYNC"));
+            break;
+        case EV_RESET:
+            Serial.println(F("EV_RESET"));
+            break;
+        case EV_RXCOMPLETE:
+            // data received in ping slot
+            Serial.println(F("EV_RXCOMPLETE"));
+            break;
+        case EV_LINK_DEAD:
+            Serial.println(F("EV_LINK_DEAD"));
+            break;
+        case EV_LINK_ALIVE:
+            Serial.println(F("EV_LINK_ALIVE"));
+            break;
+         default:
+            Serial.println(F("Unknown event"));
+            break;
+    }
 }
 
 void getData() {
@@ -154,99 +135,152 @@ void getData() {
     ahrs.getOrientation(&orientation);
 }
 
-unsigned char custom_payload[128];
+void do_send(osjob_t* j){
+    // Check if there is not a current TX/RX job running
+    if (LMIC.opmode & OP_TXRXPEND) {
+        Serial.println(F("OP_TXRXPEND, not sending"));
+    } else {
+        // Prepare data
+        Serial.println("Updating GPS and Orientation data");
+        getData();
 
-void txloop() {
-    int packet_length = construct_payload(&custom_payload[0], &orientation, rt.getTime(), gps, analogRead(PA0));
-
-    writeRegister(REG_LR_PAYLOADLENGTH, packet_length);
-
-    // Send
-    writeRegister(REG_LR_OPMODE,MODE_STANDBY);
-    writeRegister(REG_LR_FIFOTXBASEADDR , 0x00);
-    writeRegister(REG_LR_FIFOADDRPTR, 0x00);
+        // Construct packet
+        Serial.println("Constructing the packet");
+        int packet_length = construct_payload(&custom_payload[0], &orientation, rt.getTime(), gps, analogRead(PA0));
 
 #define DEBUG 1
 #if DEBUG
-    Serial.print("{[Orientation] ");
-    Serial.print("Roll: ");
-    Serial.print(orientation.roll);
-    Serial.print(", Pitch: ");
-    Serial.print(orientation.pitch);
-    Serial.print(", Heading: ");
-    Serial.print(orientation.heading);
+        // Print all sensor data
+        Serial.print("{[Orientation] ");
+        Serial.print("Roll: ");
+        Serial.print(orientation.roll);
+        Serial.print(", Pitch: ");
+        Serial.print(orientation.pitch);
+        Serial.print(", Heading: ");
+        Serial.print(orientation.heading);
+    
+        Serial.print("} {[Time] ");
+        Serial.print(rt.getTime());
+    
+        Serial.print("} {[GPS] ");
+        Serial.print("Lat: ");
+        Serial.print(gps.location.lat());
+        Serial.print(", Lng: ");
+        Serial.print(gps.location.lng());
+    
+        Serial.print("} {[Power] ");
+        Serial.print(analogRead(PA0));
+        Serial.println("}");
 
-    Serial.print("} {[Time] ");
-    Serial.print(rt.getTime());
-
-    Serial.print("} {[GPS] ");
-    Serial.print("Lat: ");
-    Serial.print(gps.location.lat());
-    Serial.print(", Lng: ");
-    Serial.print(gps.location.lng());
-
-    Serial.print("} {[Power] ");
-    Serial.print(analogRead(PA0));
-    Serial.println("}");
+        // Print data packet (in hex)
+        for (int i = 0; i < packet_length; i++) {
+            if (custom_payload[i] < 16) {
+                Serial.print("0x0");
+            } else {
+                Serial.print("0x");
+            }
+            Serial.print(custom_payload[i], HEX);
+            Serial.print(" ");
+            SPI.transfer(custom_payload[i]);
+        }
 #endif
 
-    select();
-
-    SPI.transfer(REG_LR_FIFO | 0x80);
-    for (int i = 0; i < packet_length; i++) {
-        if (custom_payload[i] < 16) {
-            Serial.print("0x0");
-        } else {
-            Serial.print("0x");
-        }
-        Serial.print(custom_payload[i], HEX);
-        Serial.print(" ");
-        SPI.transfer(custom_payload[i]);
+        // Prepare upstream data transmission at the next possible time.
+        LMIC_setTxData2(1, custom_payload, packet_length-1, 1); // last parameters for ACK (0 or 1)
+        Serial.println(F("Packet queued"));
     }
-    Serial.println();
-
-    unselect();
-
-    writeRegister(REG_LR_LNA, LNA_OFF_GAIN);        // TURN LNA OFF FOR TRANSMIT
-    writeRegister(REG_LR_PACONFIG, PA_MED_BOOST);   // TURN PA TO MAX POWER
-    writeRegister(REG_LR_OPMODE, MODE_TX);
-
-    //Serial.println("Wait for dio0 (txdone)");
-    while(digitalRead(DIO0) == 0)  {   delay(10);  } //Serial.print(". ");
-    //Serial.println("");
-
-    // clear the flags 0x08 is the TxDone flag
-    writeRegister(REG_LR_IRQFLAGS, RFLR_IRQFLAGS_TXDONE_MASK);
-
-    delay(TXDELAY);
+    // Next TX is scheduled after TX_COMPLETE event.
 }
 
-byte readRegister(byte addr)
-{
-    select();
-    SPI.transfer(addr & 0x7F);
-    byte regval = SPI.transfer(0);
-    unselect();
-    return regval;
+void setup() {
+    Serial.begin(9600);
+
+    // Startup delay
+    delay(5000);
+    Serial.println(F("Starting"));
+
+    #ifdef VCC_ENABLE
+    // For Pinoccio Scout boards
+    pinMode(VCC_ENABLE, OUTPUT);
+    digitalWrite(VCC_ENABLE, HIGH);
+    delay(1000);
+    #endif
+
+    // LMIC init
+    os_init();
+    // Reset the MAC state. Session and pending data transfers will be discarded.
+    LMIC_reset();
+
+    // Set static session parameters. Instead of dynamically establishing a session
+    // by joining the network, precomputed session parameters are be provided.
+    #ifdef PROGMEM
+    // On AVR, these values are stored in flash and only copied to RAM
+    // once. Copy them to a temporary buffer here, LMIC_setSession will
+    // copy them into a buffer of its own again.
+    uint8_t appskey[sizeof(APPSKEY)];
+    uint8_t nwkskey[sizeof(NWKSKEY)];
+    memcpy_P(appskey, APPSKEY, sizeof(APPSKEY));
+    memcpy_P(nwkskey, NWKSKEY, sizeof(NWKSKEY));
+    LMIC_setSession (0x1, DEVADDR, nwkskey, appskey);
+    #else
+    // If not running an AVR with PROGMEM, just use the arrays directly
+    LMIC_setSession (0x1, DEVADDR, NWKSKEY, APPSKEY);
+    #endif
+
+    #if defined(CFG_eu868)
+    // Set up the channels used by the Things Network, which corresponds
+    // to the defaults of most gateways. Without this, only three base
+    // channels from the LoRaWAN specification are used, which certainly
+    // works, so it is good for debugging, but can overload those
+    // frequencies, so be sure to configure the full frequency range of
+    // your network here (unless your network autoconfigures them).
+    // Setting up channels should happen after LMIC_setSession, as that
+    // configures the minimal channel set.
+    // NA-US channels 0-71 are configured automatically
+    LMIC_setupChannel(0, 868100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(1, 868300000, DR_RANGE_MAP(DR_SF12, DR_SF7B), BAND_CENTI);      // g-band
+    LMIC_setupChannel(2, 868500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(3, 867100000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(4, 867300000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(5, 867500000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(6, 867700000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(7, 867900000, DR_RANGE_MAP(DR_SF12, DR_SF7),  BAND_CENTI);      // g-band
+    LMIC_setupChannel(8, 868800000, DR_RANGE_MAP(DR_FSK,  DR_FSK),  BAND_MILLI);      // g2-band
+    // TTN defines an additional channel at 869.525Mhz using SF9 for class B
+    // devices' ping slots. LMIC does not have an easy way to define set this
+    // frequency and support for class B is spotty and untested, so this
+    // frequency is not configured here.
+    #elif defined(CFG_us915)
+    // NA-US channels 0-71 are configured automatically
+    // but only one group of 8 should (a subband) should be active
+    // TTN recommends the second sub band, 1 in a zero based count.
+    // https://github.com/TheThingsNetwork/gateway-conf/blob/master/US-global_conf.json
+    LMIC_selectSubBand(1);
+    #endif
+
+    // Disable link check validation
+    LMIC_setLinkCheckMode(0);
+
+    // TTN uses SF9 for its RX2 window.
+    LMIC.dn2Dr = DR_SF9;
+
+    // Set data rate and transmit power for uplink (note: txpow seems to be ignored by the library)
+    LMIC_setDrTxpow(DR_SF7,14);
+
+    Serial.println("Device Setup Complete");
+
+    // Initialise the sensors.
+    accel.begin();
+    mag.begin();
+    // Initialise batter analogue read
+    pinMode(PA0, INPUT_ANALOG);
+
+    Serial.println("Sensor Initialisation Complete");
+
+    // Start job
+    do_send(&sendjob);
 }
 
-
-void writeRegister(byte addr, byte value)
-{
-    select();
-    SPI.transfer(addr | 0x80); // OR address with 10000000 to indicate write enable
-    SPI.transfer(value);
-    unselect();
+void loop() {
+    os_runloop_once();
 }
-
-void select()
-{
-    digitalWrite(SSPIN, LOW);
-}
-
-
-void unselect()
-{
-    digitalWrite(SSPIN, HIGH);
-}
-
